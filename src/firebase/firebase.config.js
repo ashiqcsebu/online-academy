bc3ea3c94a1f71fa7f063bd6be3da2942ee05087
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-10-25 02:42:03 +0600

    firebase , react router dom, taiwind installed successfully

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPzyq7BbZwHhDiXO8I1RZd09ZLU3tg1Xk",
+  authDomain: "edu-hive-5ffd0.firebaseapp.com",
+  projectId: "edu-hive-5ffd0",
+  storageBucket: "edu-hive-5ffd0.appspot.com",
+  messagingSenderId: "801349978998",
+  appId: "1:801349978998:web:172c09c64b5e3edb274857"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
